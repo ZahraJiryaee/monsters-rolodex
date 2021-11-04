e8c61275c4615c65cf7e0e70d4b08dbd7817cc74
DESKTOP-EKF928S\Zahra <zahrajiryaee77@gmail.com>
2021-11-04 19:37:23 +0330

    class method & arrow funcs

--- src/App.js
+++ src/App.js
@@ -13,13 +13,33 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    /* 
+    this.handleChange = this.handleChange.bind(this);
+    
+    .bind is a method on any func that returns a func where the context of this is set to whatever we passed to it.
+    "this" is defined inside of our constructor.
+    */
   }
+
+  /*
+  handleChange(e) {
+    // js doesn't set "this" on funcs
+    this.setState({ searchField: e.target.value })
+  }
+  */
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  handleChange = (e) => {
+    // by arrow funcs we can set the context of "this" in whatever it was that declared in the first place.
+    this.setState({ searchField: e.target.value })
+  }
+  
 
   render() {
     const { monsters, searchField } = this.state;
@@ -31,7 +51,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
